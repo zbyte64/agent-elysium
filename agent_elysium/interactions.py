9f7672b9b1241781cce1aae6e67243b0a75ae43e
Jason Kraus <zbyte64@gmail.com>
2025-05-31 17:29:34 -0700

    improve markup in interaction response

--- agent_elysium/interactions.py
+++ agent_elysium/interactions.py
@@ -1,11 +1,12 @@
 def ask_player(actor:str, player_role:str, message:str) -> str:
     print(f'[{actor}] {message}')
-    return input(f'[{player_role}]: ')
+    response = input(f'[{player_role}]: ')
+    return f'[{actor} asks {player_role}] {message}\n[{player_role} responds to {actor}] {response}'
 
 
 def tell_player(actor:str, player_role:str, message:str) -> str:
     print(f'[{actor}] {message}')
-    return f'[{actor}] {message}'
+    return f'[{actor} tels {player_role}] {message}'
     
 
 def notify_player(message:str):
